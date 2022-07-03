b963cc64d8e9229beb0423c03b5add57c204afce
Bruno Pereyra <brunopereyra459@gmail.com>
2022-07-03 13:21:28 -0300

    v3 new story v1

--- src/components/stories/NewStory.js
+++ src/components/stories/NewStory.js
@@ -1,68 +1,48 @@
 import React, { useState } from "react";
-import { Avatar } from "@mui/material";
+// import { Avatar } from "@mui/material";
 import "../../static/styles/NewStory.css";
 
 export default function NewStory() {
+
   const [Textarea, setTextarea] = useState({
-    title: 1,
-    subtitle: 1,
-    vtitle: "",
-    vStitle: "",
-    ttextTitle: 83,
-    ttextSTitle: 83,
+    inputText: "",
   });
   function HandleChange(e) {
     setTextarea({
       ...Textarea,
       [e.target.name]: e.target.value,
     });
-
-    if (Textarea.vtitle.length >= Textarea.ttextTitle) {
-      setTextarea({
-        ...Textarea,
-        ttextTitle: Textarea.ttextTitle + 83,
-        title: Textarea.title + 1,
-      });
-    }
-    if (Textarea.vStitle.length >= Textarea.ttextSTitle) {
-      setTextarea({
-        ...Textarea,
-        ttextSTitle: Textarea.ttextSTitle + 83,
-        subtitle: Textarea.subtitle + 1,
-      });
-    }
-    console.log(Textarea.ttextTitle, Textarea.title);
   }
 
   return (
-    <div id="new_story">
-      <nav id="nav_new_Story">
-        <h1 id="icon_App_new_story">icon_App</h1>
-        <div id="story_access_new_story">
-          <span id="publish_button_new_story">Publish</span>
-          <Avatar>B</Avatar>
-        </div>
-      </nav>
-      <div id="make_history">
+    <div>
+
+      <div id="new_story">
         <form id="form_make_history">
-          <textarea
-            name="vtitle"
-            id="title"
-            value={Textarea.vtitle}
-            rows={Textarea.title}
-            placeholder="Title"
-            autoCapitalize="none"
-            onChange={HandleChange}
-          />
-          <textarea
-            name="vStitle"
-            id="subtitle"
-            value={Textarea.vStitle}
-            rows={Textarea.subtitle}
-            placeholder="subTitle"
-            autoCapitalize="none"
-            onChange={HandleChange}
-          />
+          <div className="div_new_story">
+            <input
+              name="inputText"
+              id="inputText"
+              value={Textarea.inputText}
+              placeholder="Title"
+              autoCapitalize="none"
+              autoFocus={true}
+              onChange={HandleChange}
+            />
+          </div>
+          <div className="div_new_story">
+            <textarea
+              name="Textarea_new_story"
+              id="writeText"
+              value={Textarea.inputText}
+              placeholder="Tell your story..."
+              autoCapitalize="none"
+              onChange={HandleChange}
+            />
+          </div>
+          <button className="writeSubmit" type="submit">
+            Publish
+          </button>
         </form>
       </div>
     </div>
